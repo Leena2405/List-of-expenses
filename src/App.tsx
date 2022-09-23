3669fc13e4432f2f7610eddfcdc09deb299450d0
Leena2405 <zaneta.lenart@gmail.com>
2022-09-23 16:31:28 +0200

    Buttons and links in menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,43 @@ import React, { FC } from 'react'
 
 import Exspenses from './components/exspenses/Exspenses'
 import { useAppSelector } from './store'
+import Button from './atoms/Button/Button'
+import { NavLink, Link } from 'react-router-dom'
+
+const buttonData = ['Wiadomości', 'Sport', 'Pogoda', 'Horoskop']
+
+const customButtonData = [
+	{
+		text: 'Wiadomości',
+		color: 'red',
+	},
+	{
+		text: 'Sport',
+		color: 'green',
+	},
+	{
+		text: 'Pogoda',
+		color: 'red',
+	},
+]
 
 const App: FC = () => {
-	const test=useAppSelector(state=>state.exspenses.listExspenses)
-	console.log(test);
-	return <Exspenses />
+	const test = useAppSelector(state => state.exspenses.listExspenses)
+	console.log(test)
+	return (
+		<>
+			<Exspenses />
+			{/* budowanie komponentów reuzywalnych, przekazywanie tekstu i np kolory, na samej tablicy i na tablicy z obiektami */}
+			{buttonData.map(element => (
+				<Button text={element} />
+			))}
+			{customButtonData.map(element => (
+				<Button text={element.text} color={element.color} />
+			))}
+			<NavLink to={'/test'}>test</NavLink>
+			<Link to='/invoices'>Invoices</Link> | <Link to='/expenses'>Expenses</Link>
+		</>
+	)
 }
 
 export default App
--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -7,11 +7,11 @@ import cn from 'classnames'
 interface ButtonProps {
 	text: string
 	onClick?: () => void
-	color: 'red' | 'green';
-	type: 'submit';
+	color?: string
+	type?: 'submit'
 }
 
-const Button: FC<ButtonProps> = ({ text, onClick, color, type}) => {
+const Button: FC<ButtonProps> = ({ text, onClick, color = 'red', type }) => {
 	return (
 		<button type={type} className={cn(styles.element, styles[`hasColor-${color}`])} onClick={onClick}>
 			{text}
